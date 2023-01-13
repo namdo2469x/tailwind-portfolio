5d484599af26f7f91a61181713f13214dfb6b271
namddoox <namdo2469x@gmail.com>
2023-01-13 13:59:57 +0700

    final 3

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,7 +24,7 @@ const Contact = () => {
             />
             <input
               type="text"
-              name="name"
+              name="email"
               placeholder="Enter your email..."
               className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             />
